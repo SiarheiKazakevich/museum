4e601f559c323bac9d75a03bd34c0f53dfb96244
Siarhei Kazakevich <dianrom@tut.by>
2025-08-07 20:55:08 +0300

    feat: finish fixing  welcome slider

--- scripts/welcome-slider.js
+++ scripts/welcome-slider.js
@@ -107,3 +107,18 @@ function prevSlide() {
   }, 600);
 }
 
+/*---------переход на сайт лувра при клике по кнопке--------*/
+document.querySelector('.welcomebutton').addEventListener('click', function () {
+  window.open('https://www.google.com/maps/@48.8618159,2.3366818,3a,75y,81.55h,73.4t/data=!3m7!1e1!3m5!1sAF1QipOVxZQuSy3Bx9T_HpH_7FtBHDTXvI6SF-A10ocT!2e10!3e12!7i5472!8i2736', '_blank');
+})
+
+const burger = document.querySelector('.burgermenu');
+const lines = document.querySelectorAll('.linesizeburgermenu');
+
+burger.addEventListener('mouseenter', () => {
+  lines.forEach(line => line.classList.add('hovered'));
+});
+
+burger.addEventListener('mouseleave', () => {
+  lines.forEach(line => line.classList.remove('hovered'));
+});
